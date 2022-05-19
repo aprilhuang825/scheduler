f87edeaa24179b0ee07258f119e04e6ca566c3a7
Ziyu <aprilhuang825@gmail.com>
2022-05-19 10:17:30 -0700

    create controlled lists

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,14 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const items = props.days.map((item) => {
+  const items = props.days.map((day) => {
     return (
       <DayListItem
-        key={item.id}
-        name={item.name}
-        spots={item.spots}
-        selected={item.name === item.day}
-        setDay={props.setDay}
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.value}
+        setDay={props.onChange}
       />
     )
   })
