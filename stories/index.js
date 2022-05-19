7955b3fde0c5cd5659dc9b1edfc3ad7c3559d923
Ziyu <aprilhuang825@gmail.com>
2022-05-19 13:30:16 -0700

    build appointment form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,8 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
+import { act } from "@testing-library/react";
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,3 +154,5 @@ storiesOf("Appointment", module)
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
   .add("Status", () => <Status message="Deleting" />)
   .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
+  .add("Form Edit", () => <Form student="April Huang" interviewer={7} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Form Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
