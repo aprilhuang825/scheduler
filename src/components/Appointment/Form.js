5ff3e6a5ac8fa40e8395e6b9f73d740c15b82fb2
Ziyu <aprilhuang825@gmail.com>
2022-05-21 14:33:39 -0700

    add reset and cancel function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,7 +5,14 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -19,14 +26,14 @@ export default function Form(props) {
           />
         </form>
         <InterviewerList
-          interviewers={props.interviewer}
+          interviewers={props.interviewers}
           value={interviewer}
           onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel} >Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
