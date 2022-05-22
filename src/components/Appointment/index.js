bca16430b7faf093f719efdb3afae49f2b8ec63c
Ziyu <aprilhuang825@gmail.com>
2022-05-21 21:16:14 -0700

    update show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,7 +7,9 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show /> : <Empty />}
+      {props.interview ? <Show 
+      student={props.interview.student}
+      interviewer={props.interview.interviewer} /> : <Empty />}
 
 
     </article>
