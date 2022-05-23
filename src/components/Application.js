afd5431ab07a18679d0bc237d448366f6fd2851f
Ziyu <aprilhuang825@gmail.com>
2022-05-23 11:11:07 -0700

    remove the days array and use useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,24 +4,6 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = {
   "1": {
     id: 1,
@@ -63,6 +45,7 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   return (
     <main className="layout">
       <section className="sidebar">
