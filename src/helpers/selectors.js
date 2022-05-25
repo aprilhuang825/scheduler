ffbf4af70def58d5d0fcfa5496a54457be05034b
Ziyu <aprilhuang825@gmail.com>
2022-05-25 10:50:12 -0700

    implement getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -15,3 +15,21 @@ export function getAppointmentsForDay(state, day) {
   }
   return appointmentsForDay;
 }
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewerId = interview.interviewer;
+
+  for (const id in state.interviewers) {
+    if (Number(id) === interviewerId) {
+      return (
+        {
+          student: interview.student,
+          interviewer: state.interviewers[id]
+        }
+      )
+    }
+  }
+}
