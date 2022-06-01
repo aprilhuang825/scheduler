baae7766d90b19babd0b114e193f9ece2ff6e82b
Ziyu <aprilhuang825@gmail.com>
2022-06-01 10:34:03 -0700

    add replace parameter to transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,9 +3,17 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = newMode => {
+  const transition = (newMode, replace = false) => {
     setMode(newMode);
-    setHistory(history => [...history, newMode]);
+    setHistory(history => {
+      if (replace) {
+        const newHistory = [...history];
+        newHistory.splice(-1, 1, newMode);
+        return newHistory;
+      } else {
+        return [...history, newMode]
+      }
+    })
   }
   const back = () => {
     setHistory(history => {
