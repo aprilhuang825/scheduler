0dec0d9beaaef7c462a95aa4c509a095ba9d2296
Ziyu <aprilhuang825@gmail.com>
2022-06-02 09:43:48 -0700

    add transition and create mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,9 +4,11 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -16,13 +18,14 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       )}
+      {mode === CREATE && (<Form interviewers={[]} />)}
     </article>
   )
 }
