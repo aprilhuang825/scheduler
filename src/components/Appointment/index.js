d0c31f34ec979ce191a6c17b1cf9add744b33b84
Ziyu <aprilhuang825@gmail.com>
2022-06-02 09:47:21 -0700

    add onCancel to return to empty state

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -25,7 +25,11 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode === CREATE && (<Form interviewers={[]} />)}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={back}
+        />)}
     </article>
   )
 }
