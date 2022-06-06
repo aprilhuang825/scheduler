dda3894d4d9976766b4461389d085f5a90328701
Ziyu <aprilhuang825@gmail.com>
2022-06-06 11:32:35 -0700

    type checking with prop types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
-
-export default function InterviewerList(props) {
+import PropTypes from 'prop-types'; 
+function InterviewerList(props) {
   const interviewerList = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
@@ -21,3 +21,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+export default InterviewerList;
