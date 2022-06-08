17512e4549ffd3824fffd0e3db0c586f104f79f3
Ziyu <aprilhuang825@gmail.com>
2022-06-08 10:51:18 -0700

    add more async test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,16 +1,29 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM } from "@testing-library/react";
+
+import { getByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
+describe("Application", () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
 
-  return waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
+
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
-});
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    console.log(prettyDOM(container));
+  })
+
+})
