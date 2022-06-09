ae34fe68872373911749de15c4c0da08b74074a9
Ziyu <aprilhuang825@gmail.com>
2022-06-08 21:04:51 -0700

    Include put in the axios mock and add more test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render, cleanup, waitForElement, fireEvent, prettyDOM } from "@testing-library/react";
 
-import { getByText } from "@testing-library/react";
+import { getByText, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -20,10 +20,22 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
-
+  
     await waitForElement(() => getByText(container, "Archie Cohen"));
-
-    console.log(prettyDOM(container));
-  })
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
+    fireEvent.click(getByAltText(appointment, "Add"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    console.log(prettyDOM(appointment));
+  });
 
 })
