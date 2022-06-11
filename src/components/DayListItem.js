a2d3104efe16d4d50799ec2e2495b0b3085ae15b
Ziyu <aprilhuang825@gmail.com>
2022-06-10 21:15:57 -0700

    add verification to previous test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,11 +3,11 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
-  const dayClass = classNames("day-list__item",{
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   })
-  const formatSpots = function(spots){
+  const formatSpots = function (spots) {
     if (spots === 0) {
       return "no spots remaining"
     }
@@ -17,12 +17,15 @@ export default function DayListItem(props) {
     }
 
     return `${spots} spots remaining`
-  } 
+  }
 
-  
+
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
+    <li className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
+      <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
